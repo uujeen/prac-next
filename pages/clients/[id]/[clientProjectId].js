da923352b88094ab55c05f524e6cbd6cdc6e53a0
uujeen <rkddmlwls2@gmail.com>
2023-11-14 18:56:07 +0900

    Feat: Udemy Next.js 강의 62 중첩된 동적 라우트 & 경로 구축하기 동적 경로에 동적 경로 구축

---
+++ pages/clients/[id]/[clientProjectId].js
@@ -0,0 +1,9 @@
+function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>The Proejct Page of Specific Project for a Seleted Client</h1>
+    </div>
+  );
+}
+
+export default SelectedClientProjectPage;
